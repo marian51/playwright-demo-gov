c448cc40fd13f19895a0e6a185246190b3c1a932
Mariusz <mariusz.jozwiak@int.pl>
2025-06-11 23:58:48 +0200

    feat: add basic tests and pages for checking if website is loaded correctly

---
+++ tests/gui/page-loading.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from '@playwright/test';
+import { MainView } from '../../page-objects/mainView';
+
+test.describe('Basic tests for checking if main website is loaded correctly', { tag: ['@loading'] }, async () => {
+  test('Checking that website is loaded correctly', async ({ page }) => {
+    const mainView: MainView = new MainView(page);
+
+    await mainView.goToMainView();
+    await mainView.waitForLoadState();
+
+    const pageStatus = await mainView.getResStatus();
+
+    // Checking page title
+    await mainView.verifyPageTitle('Portal Gov.pl');
+
+    // Checking response status
+    expect(pageStatus).toBe(200);
+
+    // Checking visibility of some elements
+    await mainView.verifyFooterLogotypesDisplayed();
+    await mainView.verifyLinkWithTextDisplayed('Zaloguj');
+  });
+});
