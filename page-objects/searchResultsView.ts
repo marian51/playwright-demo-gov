1b124f2f4ba1fda6859f6b4a5a0e5e5c9bb9cba2
Mariusz <mariusz.jozwiak@int.pl>
2025-06-12 17:19:45 +0200

    feat: add methods for filtering results and verifying results

--- page-objects/searchResultsView.ts
+++ page-objects/searchResultsView.ts
@@ -7,16 +7,31 @@ export class SearchResultsView extends BasePage {
   readonly resultsCounter: Locator;
   readonly resultsList: Locator;
 
+  private readonly administrationUnitSearchInput: Locator;
+
+  private administrationUnitSearchOption: Locator;
+
   constructor(page: Page) {
     super(page);
     this.noResultsMessage = this.page.locator('.search__empty-state');
     this.foundResultsMessage = this.page.locator('.search-results');
     this.resultsCounter = this.page.locator('.search__counter');
     this.resultsList = this.page.locator('//*[contains(@class, "search-results__list result-list")]');
+
+    this.administrationUnitSearchInput = this.page.locator('#custom-select-ministryid-dropdown').getByRole('textbox', { name: 'szukaj...' });
   }
 
   async waitForResultsLoad() {
     await this.resultsList.waitFor();
+  }
+
+  async fillAdministrationSearchInput(givenText: string) {
+    await this.administrationUnitSearchInput.fill(givenText);
+  }
+
+  async selectAdministrationUnitSearchOption(optionText: string) {
+    this.administrationUnitSearchOption = this.page.getByRole('option', { name: optionText });
+    await this.administrationUnitSearchOption.click();
   }
 
   async verifyCounterNumberIsNotZero() {
@@ -34,5 +49,21 @@ export class SearchResultsView extends BasePage {
     const resultsTexts = await this.resultsList.locator('//ul/li//a').allTextContents();
     const exist = resultsTexts.some((text) => text === givenText);
     expect(exist).toBeFalsy();
+  }
+
+  async verifyResultsHaveAdministrationUnit(administrationUnit: string) {
+    const resultsUnits = await this.resultsList.locator('//ul/li/span').allTextContents();
+    resultsUnits.forEach((unit) => {
+      const cleanedUnit = unit.split('/ ')[1].trim();
+      expect(cleanedUnit).toBe(administrationUnit.trim());
+    });
+  }
+
+  async verifyResultsDoesNotHaveAdministrationUnit(administrationUnit: string) {
+    const resultsUnits = await this.resultsList.locator('//ul/li/span').allTextContents();
+    resultsUnits.forEach((unit) => {
+      const cleanedUnit = unit.split('/ ')[1].trim();
+      expect(cleanedUnit).not.toBe(administrationUnit.trim());
+    });
   }
 }
